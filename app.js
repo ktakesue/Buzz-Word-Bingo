e844e7dc418be067f994edfd3a0c8ff17aa72540
ktakesue <ktakesue@aol.com>
2018-03-17 16:47:09 -1000

    everything works except heard is jank

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require(`body-parser`);
 
 const PORT = process.env.PORT || 6969;
 let array = [];
+let score = 0;
 
 app.use(bodyParser.json());
 app.use(
@@ -27,31 +28,58 @@ app.post("/buzzwords", (req, res) => {
   const { body } = req;
   if (array.length < 5) {
     array.push(body);
-    res.send({ success: true });
+    res.json({ success: true });
     console.log("added a new buzzword", body);
   } else {
-    res.send({ success: false });
+    res.json({ success: false });
   }
 });
 
 app.put("/buzzwords", (req, res) => {
-  console.log("editted a new buzzword");
-  res.send();
+  const { body } = req;
+  const editted = array.forEach((element, i) => {
+    if (element.buzzWord === body.buzzWord) {
+      array[i].points = Number(body.points);
+      console.log("editted a value", editted);
+      res.json({ success: true });
+    } else {
+      res.json({ success: false });
+    }
+  });
 });
 
 app.delete("/buzzwords", (req, res) => {
-  console.log("deleted a buzzword");
-  res.send();
+  const { body } = req;
+  const deleted = array.findIndex(
+    element => element.buzzWord === body.buzzWord
+  );
+  if (deleted >= 0) {
+    array.splice(deleted, 1);
+    console.log("deleted a buzzword", deleted);
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
 });
 
-app.post("/rest", (req, res) => {
+app.post("/reset", (req, res) => {
+  array = [];
+  score = 0;
   console.log("reset everything");
-  res.send();
+  res.json({ success: true });
 });
 
 app.post("/heard", (req, res) => {
-  console.log("updated the score");
-  res.send();
+  const { body } = req;
+  const heard = array.filter(element => {
+    if (element.buzzWord === body.buzzWord) {
+      score = Number(score) + Number(element.points);
+      console.log("updated the score", heard);
+      res.json({ totalScore: heard });
+    } else {
+      res.json({ totalScore: false });
+    }
+  });
 });
 
 app.listen(PORT, () => {
